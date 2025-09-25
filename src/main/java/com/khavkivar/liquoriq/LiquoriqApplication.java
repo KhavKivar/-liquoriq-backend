1ecc6ad9be95d0f0d70ec15640deddde80dbea41
Khav Kivar <sebastian.calderon@sansano.usm.cl>
2025-09-25 19:37:57 -0300

    first commit

---
+++ src/main/java/com/khavkivar/liquoriq/LiquoriqApplication.java
@@ -0,0 +1,12 @@
+package com.khavkivar.liquoriq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquoriqApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LiquoriqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khavkivar/liquoriq/LiquoriqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khavkivar.liquoriq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquoriqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
